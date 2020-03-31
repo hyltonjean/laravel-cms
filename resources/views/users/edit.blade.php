fc203ba425c6e7c698404fd7d806b9e8c230cb15
hyltonjean <hyltonjean@gmail.com>
2020-03-31 17:13:06 +0200

    Added errors messages to user.edit view.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,11 +11,19 @@
       @method('PUT')
       <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
+        <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
+          value="{{ $user->name }}">
+        @error('name')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
       </div>
       <div class="form-group">
         <label for="about">About me</label>
-        <textarea name="about" id="about" cols="5" rows="5" class="form-control">{{ $user->about }}</textarea>
+        <textarea name="about" id="about" cols="5" rows="5"
+          class="form-control @error('about') is-invalid @enderror">{{ $user->about }}</textarea>
+        @error('about')
+        <div class="alert alert-danger">{{$message}}</div>
+        @enderror
       </div>
       <button type="submit" class="btn btn-outline-success">Update Profile</button>
     </form>
